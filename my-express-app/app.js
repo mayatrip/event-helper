8e2fcb308d2cef82f7ebcd12343c7fbbbdaa9b8c
Kelly Vy Tran <kt.kellytran@gmail.com>
2023-02-22 17:47:53 +0700

    fix the bugs client and server working

--- my-express-app/app.js
+++ my-express-app/app.js
@@ -6,7 +6,11 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+const cors = require('cors');  // add at the top
+
 var app = express();
+
+app.use(cors());  // add after 'app' is created which is express
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -17,7 +21,6 @@ app.use(cookieParser());
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-const cors = require('cors');  // add at the top
-app.use(cors());  // add after 'app' is created
+
 
 module.exports = app;
