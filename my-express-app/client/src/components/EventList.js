c917a7782569f87f49d123349ed0fc94ec1f5d50
Kelly Vy Tran <kt.kellytran@gmail.com>
2023-02-23 16:33:46 +0700

    fixing bug on why proxy was not connecting

--- my-express-app/client/src/components/EventList.js
+++ my-express-app/client/src/components/EventList.js
@@ -2,13 +2,20 @@ import React from 'react';
 
 function EventList(props) {
     return (
-            {props.event.map(e => (
-                    <div key={e.id}>
-                        {e.date}
-                        {}
-                    </div>
+        <div>
+            { 
+            props.allEventsCb.map(e => (
+                <div key={e.id}>
+                {e.date}
+                {e.title} 
+                   
+                </div>
+
             ))
             }
+
+        </div>
+
     );
 }
 
