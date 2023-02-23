6e59c92d4c3d4d96ca98add3e9ec4bca6a7ba9a1
Kelly Vy Tran <kt.kellytran@gmail.com>
2023-02-23 23:55:44 +0700

    call db 3 times for the 3 insert into

--- my-express-app/routes/event.js
+++ my-express-app/routes/event.js
@@ -18,21 +18,28 @@ router.get('/', async function(req, res, next) {
 /* POST a new acitvity */
 router.post("/", async function (req, res, next) {
   //get the texts from the body
+  //I think I need to do something like select and join to have everything in the req.body
   let {date, title, deadline, activityName, description, price, link, location} = req.body;
-  let sql = `
-  INSERT INTO keyInfo(date, title, deadline)
-  VALUES ('${date}', '${title}', '${deadline}')
 
+  let insertKeyInfo = `
+  INSERT INTO keyInfo(date, title, deadline)
+  VALUES ('${date}', '${title}', '${deadline}')`;
+
+  let insertActivityOne = `
   INSERT INTO activities (activityName, description, price, link, location)
-  VALUES ('${activityName}', '${description}', '${price}', '${link}', '${location}')
+  VALUES ('${activityName}', '${description}', '${price}', '${link}', '${location}')`;
   
+  let insertActivityTwo = `
   INSERT INTO activities (activityName, description, price, link, location)
   VALUES ('${activityName}', '${description}', '${price}', '${link}', '${location}')
   `;
 
   try {
     //add new activity
-    await db(sql);
+    await db(insertKeyInfo);
+    await db(insertActivityOne);
+    await db(insertActivityTwo);
+
     let result = await db('SELECT * FROM event');
     let event = result.data;
 
