a7af2bb747215fc402dea18dc18d581cefc1ffcf
Kelly Vy Tran <kt.kellytran@gmail.com>
2023-02-27 18:48:14 +0700

    init db update

--- my-express-app/model/init_db.sql
+++ my-express-app/model/init_db.sql
@@ -5,21 +5,26 @@ CREATE TABLE keyInfo (
     keyInfo_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
     date VARCHAR(50) NOT NULL, 
     title VARCHAR(50) NOT NULL, 
-    deadline VARCHAR(50) NOT NULL
+    deadline VARCHAR(50) NOT NULL,
+    keySource_id INT NOT NULL
 );
 
 CREATE TABLE activities (
-        id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+        activities_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
         activityName VARCHAR(50) NOT NULL, 
         description VARCHAR(100) NOT NULL, 
         price INT NOT NULL, 
         link VARCHAR(500), 
         location VARCHAR(50) NOT NULL, 
         keyInfo_id INT NOT NULL,
+        keySource_id INT NOT NULL
 );
 
 CREATE TABLE keySource (
-    id INT id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+    keySource_id INT id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+    keyInfo_id INT NOT NULL,
+    activities_Id INT NOT NULL
+
 
 )
 
