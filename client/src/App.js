f92e1074be81b66f85e2c1bbfdeadbe9fadc04ff
Maya Tripathy <maya.tripathy1@gmail.com>
2023-03-01 17:22:34 +0100

    reorganized files and commented out api requests that weren't working

---
+++ client/src/App.js
@@ -0,0 +1,135 @@
+import React, { useEffect, useState } from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import './App.css';
+import AddFormEvent from "./components/AddFormEvent";
+import Dashboard from "./components/Dashboard";
+
+function App() {
+  let [allEvents, setAllEvents] = useState([]);
+  // let [allVotes, setAllVotes] = useState([]);
+
+  // useEffect() will call getEvent() when App is mounted on the DOM
+  useEffect(() => {
+    getEvent();  
+  }, []);
+  // the empty [] means only call it once
+
+  //Get all the events
+  async function getEvent() {
+    try {
+      let response = await fetch('/event');
+      if (response.ok) {
+        let data = await response.json();
+        setAllEvents(data);
+      } else {
+        console.log(`Server error: ${response.status}: ${response.statusText}`);
+      }
+    } catch(err) {
+      console.log(`Network error: ${err.message}`);
+    }
+  }
+  // useEffect() will call getEvent() when App is mounted on the DOM
+  // useEffect(() => {
+  //   getUniqueEvent();  
+  // }, []);
+
+  // Get all the events by ID
+  // async function getUniqueEvent(id) {
+  //   try {
+  //     let response = await fetch(`/event/${id}`);
+  //     if (response.ok) {
+  //       let data = await response.json();
+  //       setAllEvents(data[0]);
+  //     } else {
+  //       console.log(`Server error: ${response.status}: ${response.statusText}`);
+  //     }
+  //   } catch(err) {
+  //     console.log(`Network error: ${err.message}`);
+  //   }
+  // }
+
+  //Post a new event
+  async function addEventForm(event) {
+    //create a copy of my event object
+    //then edit that copy so that the price property has a value that correspond to a number and not a string
+    let newEvent = {...event};
+    newEvent.price = Number(newEvent.price); //reference that element to become the new one
+
+    let options = {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify(newEvent)//name of the copy
+    };
+
+    try {
+      let response = await fetch('/event', options); //do post
+      if (response.ok) {
+        let event = await response.json()
+        setAllEvents(event);
+      } else {
+        console.log(`Server error: ${response.status} ${response.statusText}`);
+      }
+    }catch (err) {
+      console.log(`Server error: ${err.message}`);
+    }
+  }
+
+//   // PUT: Add a vote to voting count
+//   async function addVote(id) {
+//     // Find activities in state and increase the voting count
+//     let vote = allVotes.find(v => v.id === id);
+//     vote.count++;
+
+//     // Define fetch() options
+//     let options = {
+//         method: 'PUT',
+//         headers: { 'Content-Type': 'application/json' },
+//         body: JSON.stringify(vote)
+//     };
+
+//     try {
+//         let response = await fetch(`/event/:activities_id`, options);  // do PUT
+//         if (response.ok) {
+//             let votes = await response.json();
+//             setAllVotes(votes);
+//         } else {
+//             console.log(`Server error: ${response.status} ${response.statusText}`);
+//         }
+//     } catch (err) {
+//         console.log(`Server error: ${err.message}`);
+//     }
+// }
+
+  return (
+
+    <div className="App">
+      <header>
+        <h1>NUGGETS EVENT</h1>
+      </header>
+
+      <nav>
+        <Link to="/">Home</Link> <Link to="/dashboard">Dashboard</Link>
+      </nav>
+
+      <div>
+        {/* for this to work, remember to import { Routes, Route} */}
+        <Routes>
+          <Route path="/" element={<AddFormEvent 
+            addEventFormCb={addEventForm}  
+            />} 
+          />
+
+          <Route path="/dashboard" element={<Dashboard 
+            allEvents={allEvents}
+            // addVoteCb1={addVote}
+            />} 
+          />
+        </Routes>
+  
+      </div>
+    </div>
+
+  );
+}
+
+export default App;
---
+++ model/database.js
@@ -0,0 +1,35 @@
+require("dotenv").config();
+const mysql = require("mysql");
+const fs = require("fs");
+
+const DB_HOST = process.env.DB_HOST;
+const DB_USER = process.env.DB_USER;
+const DB_PASS = process.env.DB_PASS;
+const DB_NAME = process.env.DB_NAME;
+
+const con = mysql.createConnection({
+  host: DB_HOST || "127.0.0.1",
+  user: DB_USER || "root",
+  password: DB_PASS,
+  database: DB_NAME || "event_planner",
+  multipleStatements: true
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+
+    // let sql = "DROP TABLE if exists event; CREATE TABLE event(id INT NOT NULL AUTO_INCREMENT, date VARCHAR(50) NOT NULL, title VARCHAR(50) NOT NULL, deadline VARCHAR(50) NOT NULL, activityName VARCHAR(50) NOT NULL, description VARCHAR(100) NOT NULL, price INT NOT NULL, link VARCHAR(500), location VARCHAR(50) NOT NULL, PRIMARY KEY(id));";
+
+  let sql = fs.readFileSync(__dirname+"/init_db.sql").toString();
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("Table creation `event` was successful!");
+
+    console.log("Closing...");
+  });
+
+  con.end();
+});
+
+
