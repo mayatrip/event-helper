7191962940d067af713f57476598ab7f1c13d187
Maya Tripathy <maya.tripathy1@gmail.com>
2023-03-02 12:29:51 +0100

    created PrivateRoute component to redirect user to login if anonymous or to allow to see pages if logged in

---
+++ client/src/components/PrivateRoute.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Navigate } from "react-router-dom";
+import Local from '../helpers/Local';
+
+export default function PrivateRoute(props) {
+  //Redirect to /login if anonymous user
+  let userId = Local.getUserId();
+  if (!userId) {
+    return <Navigate to="/login" />;
+  }
+
+  //Render child component(s)
+  return (
+    <>
+        {props.children}
+    </>
+  )
+}
