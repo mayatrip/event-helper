4a5c6d86d4af099d6e4c4462ad6dad7a43cf6e15
Maya Tripathy <maya.tripathy1@gmail.com>
2023-03-06 17:03:27 +0100

    key info with each associated activity now displays

--- client/src/views/DashboardView.js
+++ client/src/views/DashboardView.js
@@ -1,10 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Api from '../helpers/Api';
 
 function DashboardView(props) {
+const[allEvents, setAllEvents] = useState([]);
+const [allKeyInfo, setKeyInfo] = useState([]);
+
+
+useEffect(() => {
+    if(props.user) {
+      getEvents();
+      getKeyInfo();
+    }
+  }, []);
+
+async function getEvents() {
+    let uresponse = await Api.getContent('/events');
+    if (uresponse.ok){
+      setAllEvents(uresponse.data);
+    } else{
+      console.log(`Error! ${uresponse.error}`);
+    }
+  }
+
+  async function getKeyInfo() {
+    let uresponse = await Api.getContent('/keyInfo');
+    if (uresponse.ok){
+      setKeyInfo(uresponse.data);
+    } else{
+      console.log(`Error! ${uresponse.error}`);
+    }
+  }
 
 const handleClick = (id) => {
     console.log(id);
-    let selectedEvent = props.allEvents.find(i => i.activities_id === id);
+    let selectedEvent = allEvents.find(i => i.activities_id === id);
     let newCount = selectedEvent.votes + 1;
     let newAttending = selectedEvent.attending + props.user.username;
     let voteObj = {count: newCount, attending: newAttending};
@@ -14,14 +43,14 @@ const handleClick = (id) => {
 
   return (
     <div className='DashboardView'>
-        <h1>Dashboard</h1>
         {
-            props.allEvents.map(e => (
+            allEvents.map(e => (
                 <div key={e.activities_id}>
                     <div className='subGrid'>
                         <h2>To remember</h2>
-                            <p>Save the date for title on date</p>
-                            <p>Answer before </p>
+                            <p>Save the date for {(allKeyInfo.find(i => i.keyInfo_id === e.keyInfo_id)) ? (allKeyInfo.find(i => i.keyInfo_id === e.keyInfo_id)).title : ""} on {(allKeyInfo.find(i => i.keyInfo_id === e.keyInfo_id)) ? (allKeyInfo.find(i => i.keyInfo_id === e.keyInfo_id)).date : ""}
+                            </p>
+                            <p>Answer before {(allKeyInfo.find(i => i.keyInfo_id === e.keyInfo_id)) ? (allKeyInfo.find(i => i.keyInfo_id === e.keyInfo_id)).deadline : ""}</p>
                         <h2>Activity Info</h2>
                             <ul>
                             <li>{e.activityName}</li>
